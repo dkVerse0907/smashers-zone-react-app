c536c1d9fd3506c3be8bf7b1d506abc5cd7f63e5
DinSat0907 <dineshkumar@innocloud.co.in>
2025-09-13 18:02:15 +0530

    New changes

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+
+
+function Login() {
+
+  return (
+    <>
+    <div className='w-auto border-2 items-center  mt-60 p-10'>
+      <h1 className="text-3xl font-bold underline overflow-hidden">Login page</h1>
+    </div>
+    </>
+  )
+}
+
+export default Login;
